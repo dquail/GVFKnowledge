3564e3c95c794de294d8145e162b0f0c752a6e36
David Quail <quail.david@gmail.com>
2018-10-12 22:02:31 -0600

    Progress

--- python/StateRepresentation.py
+++ python/StateRepresentation.py
@@ -5,10 +5,11 @@
 """
 import numpy as np
 from tiles import *
-
+import time
 
 # image tiles
-NUM_RANDOM_POINTS = 100
+#NUM_RANDOM_POINTS = 100
+NUM_RANDOM_POINTS = 2
 CHANNELS = 4
 NUM_IMAGE_TILINGS = 8
 NUM_IMAGE_INTERVALS = 4
@@ -17,38 +18,104 @@ SCALE_RGB = NUM_IMAGE_INTERVALS / 256.0
 IMAGE_START_INDEX = 0
 
 # constants relating to image size recieved
+"""
 IMAGE_HEIGHT = 480  # rows
 IMAGE_WIDTH = 640  # columns
+"""
+IMAGE_HEIGHT = 10  # rows
+IMAGE_WIDTH = 10  # columns
 
 NUMBER_OF_CHANNELS = 3 #red, blue, green
 PIXEL_FEATURE_LENGTH = np.power(NUMBER_OF_CHANNELS, NUM_IMAGE_INTERVALS) * NUM_IMAGE_TILINGS
-TOTAL_FEATURE_LENGTH = PIXEL_FEATURE_LENGTH * NUM_RANDOM_POINTS
+DID_BUMP_FEATURE_LENGTH = 0
+TOTAL_FEATURE_LENGTH = PIXEL_FEATURE_LENGTH * NUM_RANDOM_POINTS + DID_BUMP_FEATURE_LENGTH
 
 # Channels
 RED_CHANNEL = 0
 GREEN_CHANNEL = 1
 BLUE_CHANNEL = 2
 DEPTH_CHANNEL = 3
+OBS_KEY = 'RGBD_INTERLEAVED'
 
 class StateRepresentation(object):
   def __init__(self):
     self.pointsOfInterest = []
     self.randomYs = np.random.choice(IMAGE_HEIGHT, NUM_RANDOM_POINTS, replace=False)
-    print("Random Ys")
-    print(self.randomYs)
     self.randomXs = np.random.choice(IMAGE_WIDTH, NUM_RANDOM_POINTS, replace=False)
-    print("RandomXs")
-    print(self.randomXs)
+
     for i in range(NUM_RANDOM_POINTS):
       point = self.randomXs[i], self.randomYs[i]
       self.pointsOfInterest.append(point)
     print("Points of interest:")
     print(self.pointsOfInterest)
 
+  def didBump(self, observation):
+    """
+    didBump = False
+    rgbdObs = observation[OBJ_KEY]
+    depthValues = rgbdObs[DEPTH_CHANNEL, :, :]
+    if (0 in depthValues):
+      didBump = True
+    #print("Depth values:" )
+    #print(depthValues)
 
+    print("Didbump: ")
+    print(didBump)
+
+    return didBump
+    """
+    #time.sleep(0.2)
+    obs = observation[OBS_KEY]
+    pix = obs[0,0]
+    print("Pixel One : " + str(pix))
+    pix = obs[IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2]
+    print("Pixel Mid : " + str(pix))
+    time.sleep(0.01)
+
+    didBump = False
+    obs = observation[OBS_KEY]
+    depths = obs[:,:, DEPTH_CHANNEL]
+    #print("depths: ")
+    #print(depths)
+    minD = np.amin(depths)
+    if minD == 0:
+      print("Depth 0. Sleeping ...")
+      time.sleep(5)
+    maxD = np.amax(depths)
+    avgD = np.average(depths)
+    midPix = depths[IMAGE_HEIGHT / 2][IMAGE_WIDTH / 2]
+    print("Min depth: " + str(minD))
+    print("Max depth: " + str(maxD))
+    print("average depth: " + str(avgD))
+    print("mid point depth" + str(midPix))
+    print("")
+    #print("MinD: " + str(minD))
+    #print("MaxD: " + str(maxD))
+    didBump = minD < 160
+
+    if didBump:
+      print("BUMPED!!!!!")
+      time.sleep(0.5)
+
+    """
+    depthArray = []
+    #width, height
+    for x in range(3):
+      for y in range(2):
+        depth = obs[DEPTH_CHANNEL, y,x]
+        #print("x: " + str(x) + ", y: " + str(y) + ", depth: " + str(depth))
+        depthArray.append(depth)
+    if (0 in depthArray):
+      didBump = True
+    """
+    #print("Did bump: " + str(didBump))
+
+    return didBump
 
   def getPhi(self, observation):
-    rgbdObs = observation['RGBD']
+    if not observation:
+      return None
+    rgbdObs = observation[OBS_KEY]
     #tilecode each pixel indivudually and then assemble
 
     phi = []
@@ -57,9 +124,9 @@ class StateRepresentation(object):
       #Get the pixel value at that point
       x = point[0]
       y = point[1]
-      red = rgbdObs[RED_CHANNEL, y, x] / 256.0
-      green = rgbdObs[GREEN_CHANNEL, y, x] / 256.0
-      blue = rgbdObs[BLUE_CHANNEL, y, x] / 256.0
+      red = rgbdObs[y, x, RED_CHANNEL] / 256.0
+      green = rgbdObs[y, x, GREEN_CHANNEL] / 256.0
+      blue = rgbdObs[y, x, BLUE_CHANNEL] / 256.0
 
       pixelRep = np.zeros(PIXEL_FEATURE_LENGTH)
       #Tile code these 3 values together
@@ -69,6 +136,9 @@ class StateRepresentation(object):
 
       #Assemble with other pixels
       phi.append(pixelRep)
+
+      didBump = self.didBump(observation)
+
 
     return phi
 
--- python/learning_agent (new).py
+++ python/learning_agent (new).py
@@ -15,31 +15,30 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """Basic random agent for DeepMind Lab."""
 
-# Basic imports
+#Basic imports
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 
 import argparse
 import random
 import numpy as np
 import six
 import time
-from matplotlib import pyplot as PLT
 
-# Project imports
+#Project imports
 from GVF import *
 from BehaviorPolicy import *
 from StateRepresentation import *
 
-# Deepmind import
+#Deepmind import
 import deepmind_lab
-
 
 class LearningForeground:
   def __init__(self, length, width, height, fps, level, record, demo, video):
 
-    # Set up our environment
+    #Set up our environment
     config = {
       'fps': str(fps),
       'width': str(width),
@@ -51,22 +50,22 @@ class LearningForeground:
       config['demo'] = demo
     if video:
       config['video'] = video
-    self.env = deepmind_lab.Lab(level, [OBS_KEY], config=config)
+    self.env = deepmind_lab.Lab(level, ['RGBD'], config=config)
     self.env.reset()
-
-    # Set our behavior policy
+    
+    #Set our behavior policy
     self.behaviorPolicy = BehaviorPolicy()
 
     self.featureRepresentationLength = 100
     alpha = 0.1
     numberOfActiveFeatures = 5
-    # Set up our GVF for pavlovian control
-    gvf = GVF(self.featureRepresentationLength, alpha / numberOfActiveFeatures, isOffPolicy=False, name="Hit wall GVF")
+    #Set up our GVF for pavlovian control
+    gvf = GVF(self.featureRepresentationLength, alpha / numberOfActiveFeatures, isOffPolicy = False, name = "Hit wall GVF")
     gvf.gamma = 0
     gvf.policy = self.behaviorPolicy
     self.wallGVF = gvf
 
-    # Set learning parameters
+    #Set learning parameters
     self.previousObs = False
     self.currentObs = False
 
@@ -76,13 +75,14 @@ class LearningForeground:
     if observation == None:
       return None
     else:
-      # TODO: tilecode the observation RGB bits.
+      #TODO: tilecode the observation RGB bits.
       return np.zeros(self.featureRepresentationLength)
 
   def updateGVFs(self):
+    print("TBD: GVF learning")
     gvf = self.wallGVF
 
-  def start(self, numberOfSteps=10000, numberOfRuns=1):
+  def start(self, numberOfSteps = 10000, numberOfRuns = 1):
     for run in range(numberOfRuns):
       print("RUN NUMBER: " + str(run + 1) + " ......")
       self.env.reset()
@@ -95,45 +95,38 @@ class LearningForeground:
       hasPreviousObs = False
 
       for step in range(numberOfSteps):
-        # TODO - make willHitWall a pavlovian prediction
+        #TODO - make willHitWall a pavlovian prediction
         willHitWall = False
 
-        # Pavlovian control
+        #Pavlovian control
         action = None
         if willHitWall == True:
           action = self.behaviorPolicy.turnLeftPolicy(self.previousObs)
         else:
           action = self.behaviorPolicy.policy(self.previousObs)
 
-        print("Action: " + str(action))
+        print()
         self.currentAction = action
 
         reward = self.env.step(action, num_steps=1)
 
-        # Observation, for RGBD, has 'shape': (180, 320, 4)} ie. env.observations[0][0] would return an array of 4 elements. a[R,G,B,D]
+        #Observation, for RGBD, has 'shape': (180, 320, 4)} ie. env.observations[0][0] would return an array of 4 elements. a[R,G,B,D]
 
-
+        """
         if hasPreviousObs:
           self.previousObs = self.currentObs
           self.previousPhi = self.currentPhi
-        self.currentObs = self.env.observations()
+
         hasPreviousObs = True
 
-        """
-        if(step > 10):
-          print("Keys: " + str(self.currentObs.keys()))
-          time.sleep(1)
-          M = self.currentObs[OBS_KEY]
-          PLT.imshow(M)
-          PLT.show()
-        """
+        self.currentObs = self.env.observations()
+
         self.currentPhi = self.stateRepresentation.getPhi(self.currentObs)
 
-        #time.sleep(0.1)
+        time.sleep(0.1)
         #Learn
         self.updateGVFs()
-
-
+        """
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description=__doc__)
@@ -162,8 +155,8 @@ if __name__ == '__main__':
     deepmind_lab.set_runfiles_path(args.runfiles_path)
 
   foreground = LearningForeground(args.length, args.width, args.height, args.fps, args.level_script,
-                                  args.record, args.demo, args.video)
-  foreground.start(numberOfSteps=args.length, numberOfRuns=1)
+      args.record, args.demo, args.video)
+  foreground.start(numberOfSteps = args.length, numberOfRuns = 1)
 
   """
   run(args.length, args.width, args.height, args.fps, args.level_script,
